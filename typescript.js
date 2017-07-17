f7106884ac81030ed9d97ee30273f86a4f0d43bf
Jason <rhuidean06@gmail.com>
2017-07-17 12:39:19 -0500

    add typescript excercise

---
+++ typescript.js
@@ -0,0 +1,10 @@
+var msString = 'Hello world';
+var myNumber = 4;
+var anything = [];
+var myBoolean = true;
+var myArr = [1, 2, 3, 4];
+function test(yourName, options) {
+}
+test('Cody', {});
+/*tsc typescript.ts */
+/*VIM typescript.ts*/ 
---
+++ typescript.ts
@@ -0,0 +1,17 @@
+let msString: string = 'Hello world';
+let myNumber: number =4;
+
+let anything: any = [];
+
+let myBoolean: boolean = true;
+
+let myArr: number[] = [1,2,3,4];
+
+function test(yourName: string, options?: object):void{
+    console.log('testing test')
+}
+
+test('Cody',{})
+/*tsc typescript.ts */
+/*VIM typescript.ts*/
+/*number any void */
